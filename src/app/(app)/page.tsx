bc6d79b3dc755a023fb4e9df70a0372051e33870
Evandro Calado <evandrocalado07@gmail.com>
2025-01-05 00:10:03 -0300

    feat(pages): update styling for Dashboard and Home pages for improved responsiveness

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,8 @@
 export const HomePage = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full gap-6 h-4/5'>
-      <div className='flex items-center justify-center w-1/3 h-24 rounded-full bg-gradient-to-r from-zinc-500 via-zinc-600 to-zinc-700'>
-        <h2 className='text-4xl font-bold text-transparent bg-white bg-clip-text'>
+      <div className='flex items-center justify-center w-4/5 h-10 rounded-full md:w-1/3 md:h-24 bg-gradient-to-r from-zinc-500 via-zinc-600 to-zinc-700'>
+        <h2 className='text-xl font-bold text-transparent bg-white md:text-4xl bg-clip-text'>
           Home Page
         </h2>
       </div>
